f630c521a0a0644df87848d2024b09ce9a617a71
adedwi1808 <ardhikayoviyanto@gmail.com>
2022-05-22 11:04:42 +0700

    Menyelesaikan fungsi updateuser khususnya profile update dan menambahkan extension agar dapat menlakukan request ke server dalma bentuk multipartbody

--- app/src/main/java/com/example/geekgarden_attendance/ui/more/MoreViewModel.kt
+++ app/src/main/java/com/example/geekgarden_attendance/ui/more/MoreViewModel.kt
@@ -6,8 +6,4 @@ import androidx.lifecycle.ViewModel
 
 class MoreViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/geekgarden_attendance/ui/updateProfile/UpdateProfileActivity.kt
+++ app/src/main/java/com/example/geekgarden_attendance/ui/updateProfile/UpdateProfileActivity.kt
@@ -1,9 +1,11 @@
 package com.example.geekgarden_attendance.ui.updateProfile
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
 import com.example.geekgarden_attendance.R
@@ -11,6 +13,8 @@ import com.example.geekgarden_attendance.core.data.source.remote.network.State
 import com.example.geekgarden_attendance.core.data.source.remote.request.UpdateProfileRequest
 import com.example.geekgarden_attendance.databinding.ActivityUpdateProfileBinding
 import com.example.geekgarden_attendance.util.Prefs
+import com.github.drjacky.imagepicker.ImagePicker
+import com.squareup.picasso.Picasso
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class UpdateProfileActivity : AppCompatActivity() {
@@ -25,14 +29,32 @@ class UpdateProfileActivity : AppCompatActivity() {
         _binding = ActivityUpdateProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        buttonAction()
         setToolBar()
         setData()
-        buttonAction()
     }
 
     fun buttonAction(){
         binding.buttonEdit.setOnClickListener {
             updateUser()
+        }
+
+        binding.imageViewProfile.setOnClickListener {
+            picImage()
+        }
+    }
+
+    private fun picImage() {
+        ImagePicker.with(this)
+            .maxResultSize(1080, 1080, true)
+            .createIntentFromDialog { launcher.launch(it) }
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val uri = it.data?.data!!
+            // Use the uri to load the image
+            Picasso.get().load(uri).into(binding.imageViewProfile)
         }
     }
 
@@ -47,11 +69,13 @@ class UpdateProfileActivity : AppCompatActivity() {
         val user = Prefs.getUser()
 
         if (user != null) {
+            val userNameInitial = user.name?.split(' ')?.mapNotNull { it.firstOrNull()?.toString() }?.reduce { acc, s -> acc + s }
             binding.apply {
                 textInputEditUsername.setText(user.name)
 //                textViewPosisi.text = "Belum ada"
                 textInputEditEmail.setText(user.email)
                 textInputEditPhone.setText(user.phone)
+                textViewNameInitial.setText(userNameInitial)
             }
         }
     }
@@ -76,10 +100,7 @@ class UpdateProfileActivity : AppCompatActivity() {
                 State.SUCCES -> {
                     Toast.makeText(this, "Selamat Datang ${it?.data?.name}", Toast.LENGTH_SHORT).show()
                     binding.progressBar.isVisible = false
-//                    val intent = Intent(this, NavigationActivity::class.java)
-//                    intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
-//                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//                    startActivity(intent)
+                    onBackPressed()
                 }
                 State.ERROR -> {
                     Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
